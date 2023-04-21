793e820bd08fc1e7cef0ae09c81fd91e4858fe40
gwbaik9717@icloud.com <gwbaik9717@icloud.com>
2023-04-21 11:47:12 +0900

    StPageFlip 라이브러리 적용
    - 샘플 페이지 속지 추가

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Page from "../Page";
 import Sidebar from "../Sidebar";
 import styled from "styled-components";
+import HTMLFlipBook from "react-pageflip";
 
 const StyledEditor = styled.div`
   display: flex;
@@ -10,7 +11,19 @@ const StyledEditor = styled.div`
 const Editor: React.FC = () => {
   return (
     <StyledEditor>
-      <Page />
+      {/* @ts-expect-error: 라이브러리 타입 정의 오류 */}
+      <HTMLFlipBook
+        width={550}
+        height={733}
+        maxShadowOpacity={0.5}
+        usePortrait={false}
+      >
+        <Page pageNumber={1} />
+        <Page pageNumber={2} />
+        <Page pageNumber={3} />
+        <Page pageNumber={4} />
+      </HTMLFlipBook>
+      {/* <Page /> */}
       <Sidebar />
     </StyledEditor>
   );
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import styled from "styled-components";
 import { useAppDispatch, useAppSelector } from "../../store/config";
 import type { ImageElement as ImageElementType } from "../Designs/ImageElement/imageElement";
@@ -6,13 +6,16 @@ import { ElementType } from "../Designs/Element/element";
 import ImageElement from "../Designs/ImageElement";
 import { deactivateAllElements } from "../../store/slices/pageSlice";
 
+interface Props {
+  pageNumber: number;
+}
+
 const StyledPage = styled.div`
-  background-color: yellow;
-  width: 1120px;
-  height: 600px;
+  background-image: url("https://img.freepik.com/free-photo/white-texture_1160-786.jpg?w=2000&t=st=1682045423~exp=1682046023~hmac=6b3dab945e937032ea7535af0e4456d1a93711323f332731173534095e62f69d");
+  background-size: cover;
 `;
 
-const Page: React.FC = () => {
+const Page = forwardRef(function Page({ pageNumber }: Props, ref: any) {
   const { elements } = useAppSelector((state) => state.page);
   // const dispatch = useAppDispatch();
 
@@ -26,12 +29,13 @@ const Page: React.FC = () => {
   };
 
   return (
-    <StyledPage className="diary_page" onClick={handleClick}>
+    <StyledPage className="diary_page" onClick={handleClick} ref={ref}>
+      <p>{pageNumber}</p>
       {imageElements.map((element) => (
         <ImageElement key={element.id} element={element} />
       ))}
     </StyledPage>
   );
-};
+});
 
 export default Page;
